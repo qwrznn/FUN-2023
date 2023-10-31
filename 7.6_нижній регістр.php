571a15145a86a279820a16826fa5cf0aafccaabc
qwrznn <145158478+qwrznn@users.noreply.github.com>
2023-10-31 11:30:42 +0200

    Update 7.6_нижній регістр.php

--- 7.6_нижній регістр.php
+++ 7.6_нижній регістр.php
@@ -1,14 +1,5 @@
 <?php
 
-// function my_cmp($a, $b) {
-//     if ($a > $b)
-//         return 1;
-//     if ($a == $b)
-//         return 0;
-//     else
-//         return -1;
-// }
-
 $arr = ["M", "a", "r", "Y", "S", "m", "i", "T", "h"];
 
 function my_case($a) {
